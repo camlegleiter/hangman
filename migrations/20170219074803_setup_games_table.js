f71c2c89192ac602619072c62bb2e544de485ab0
camlegleiter <camlegleiter@gmail.com>
2017-08-28 08:25:18 -0500

    Remove gameService require from migrate script
    Knex.js does not understand certain ES6 syntax, including "import" statements. To avoid `knex migrate` issues,
    the gameService require has been removed from the migrate script since all it needed was the GAME_STATE
    enumeration.
    
    This should be revisited to have the state live in a separate location from gameService and this migration.
    That way the state can be shared between the two files without having these kinds of issues.

--- migrations/20170219074803_setup_games_table.js
+++ migrations/20170219074803_setup_games_table.js
@@ -1,5 +1,7 @@
 const values = require('lodash/values');
-const GAME_STATE = require('../src/services/gameService').GAME_STATE;
+// XXX: Need to pull this out into a separate location.
+//      knex does not like ES6 yet, so it cannot be transpiled
+const GAME_STATE = ['started', 'won', 'lost'];
 
 exports.up = (knex) => Promise.all([
   knex.schema.createTable('games', (t) => {
@@ -8,7 +10,7 @@ exports.up = (knex) => Promise.all([
     t.string('lettersGuessed').notNull();
     t.string('lettersMatched').notNull();
     t.integer('remainingGuesses').notNull();
-    t.enu('state', values(GAME_STATE)).notNull();
+    t.enu('state', GAME_STATE).notNull();
     t.string('word').notNull();
     t.timestamp('createdOn').notNull().defaultTo(knex.fn.now());
     t.timestamp('updatedOn').notNull().defaultTo(knex.fn.now());
